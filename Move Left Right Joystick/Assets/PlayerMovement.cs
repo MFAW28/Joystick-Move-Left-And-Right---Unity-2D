1b72cc89c4a7c5d6d39a32d9540caa0db20c1112
MFAW28 <mfaisalaw2804@gmail.com>
2021-08-22 14:40:48 +0700

    Add 22 Agustus 2021

---
+++ Move Left Right Joystick/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private Vector2 movement;
+    public float speed;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
